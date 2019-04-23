2b1a3638fafe12076736c8beb6f8538c8d49dd0c
unknown <caylenepicache@gmail.com>
2019-04-22 22:29:37 -0700

    trying to fix hello world

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import './style.css';
 
 function component() {
-    let element = document.createElement('div');
+    const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
