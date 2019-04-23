9936cc0471067e6cc47ad93bd90fa8cbf5ae675a
unknown <caylenepicache@gmail.com>
2019-04-22 23:05:42 -0700

    ridding of unnecessary files for development section

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import _ from 'lodash';
-import '../src/style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    const myIcon = newImage();
-    myIcon.src = Icon;
-
-    console.log(Data);
-  
+    
     return element;
   }
   
