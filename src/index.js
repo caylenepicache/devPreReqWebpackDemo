ff4de513f604fcdd0a1b11474b89cf3d5dcf8dc9
unknown <caylenepicache@gmail.com>
2019-04-22 22:52:31 -0700

    images and font

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import '../src/style.css';
+import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
@@ -7,6 +8,9 @@ function component() {
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    const myIcon = newImage();
+    myIcon.src = Icon;
   
     return element;
   }
