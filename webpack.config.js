4179fccb1e950a53c19ed0256fac83c73cbfcacc
unknown <caylenepicache@gmail.com>
2019-04-23 23:09:36 -0700

    entry points and prevent dup w optimization:

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,18 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
-      app: './src/index.js',
-      print: './src/print.js'
+    index: './src/index.js',
+   another: './src/another-module.js'
   },
-  devtool: 'inline-source-map',
-  devServer: {
-      contentBase: './dist'
-  },
-  plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-          title: 'Output Management'
-      })
-  ],
   output: {
-    filename: 'main.js',
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+      splitChunks: {
+          chunks: 'all'
+      }
   }
 };
