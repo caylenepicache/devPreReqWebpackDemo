c402ea2e6426bee20e63a1433d70f9705d0e41c7
unknown <caylenepicache@gmail.com>
2019-04-23 22:43:25 -0700

    using clean webpack plug in and trying to find source map of error

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
       app: './src/index.js',
       print: './src/print.js'
   },
+  devtool: 'inline-source-map',
+  plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+          title: 'Output Management'
+      })
+  ],
   output: {
     filename: 'main.js',
     filename: '[name].bundle.js',
